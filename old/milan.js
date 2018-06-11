b40e96706463e7110b34b7ed065093a62d87ab6d
themilanguy1 <milan.gupta@ymail.com>
2018-06-11 12:11:49 +0200

    e

---
+++ old/milan.js
@@ -0,0 +1,18 @@
+function HelloWorld()
+{
+ var mijnnaam = "Thot";
+ alert ("Hallo: " + mijnnaam);
+}
+
+function EenFunctie() {
+    naam = prompt("Wat is je naam?", "");
+    cancel = "geannuleerd";
+    id = ;
+    if (naam==null) 
+    {
+     naam = cancel;
+     }
+    alert ('Hallo ' + naam + ". Jij hebt geklikt op afbeelding" + id);
+}
+
+
